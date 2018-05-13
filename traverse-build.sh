188ff75c57e1e3dbd4fa37168cc5c6b2df4e6acb
Mathew McBride <matt@traverse.com.au>
2018-05-13 04:36:38 +0000

    build ngfw-upstream with this as well

--- traverse-build.sh
+++ traverse-build.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
-for i in `cat traverse-build-order.txt | awk '{{print $1}}'`; do 
+#!/bin/bash
+for i in `cat traverse-build-order.txt | awk '{{print $1}}'`; do
 	echo "Building $i"
+	echo
+	echo
+	cd $i
+	dpkg-buildpackage
+	cd ..
+	echo "DONE $i"
+	echo
+	echo
+done
+cd ngfw_upstream
+for i in $(cat build-order.txt | grep -v '#' | grep -v '^\$' | awk '{{print $1}}'); do
+	echo "Building upstream $i"
 	echo
 	echo
 	cd $i
