fd3a4a2f9157b69c9a866b9301257e7a6d159df3
Mathew McBride <matt@traverse.com.au>
2018-05-13 02:26:47 +0000

    add repo upload via ssh

---
+++ traverse-upload.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "${SSH_UPLOAD_PORT}" -eq 22 ] || [ -z "${SSH_UPLOAD_PORT+x}" ]; then
+	echo "${SSH_UPLOAD_HOST} ${SSH_UPLOAD_HOST_KEY}" > upload_host_key
+else
+	echo "[${SSH_UPLOAD_HOST}]:${SSH_UPLOAD_PORT} ${SSH_UPLOAD_HOST_KEY}" > upload_host_key
+fi
+
+echo "${SSH_UPLOAD_KEY}" > upload_user_key
+chmod 0600 upload_user_key 
+scp -o 'HashKnownHosts no' -o 'UserKnownHostsFile upload_host_key' -P "${SSH_UPLOAD_PORT}" -i upload_user_key -r repo "${SSH_UPLOAD_USER}@${SSH_UPLOAD_HOST}:${SSH_UPLOAD_DIR}"
