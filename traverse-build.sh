36bd3ba1164fb4c246673d451afa353fca548652
Mathew McBride <matt@traverse.com.au>
2018-05-13 07:40:54 +0000

    (traverse) fix build path issue

--- traverse-build.sh
+++ traverse-build.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-for i in `cat traverse-build-order.txt | awk '{{print $1}}'`; do
+THIS_DIR=$(pwd)
+for i in $(cat traverse-build-order.txt | awk '{{print $1}}'); do
 	echo "Building $i"
 	echo
 	echo
-	cd $i
+	cd "${THIS_DIR}/${i}"
 	dpkg-buildpackage
-	cd ..
 	echo "DONE $i"
 	echo
 	echo
 done
-cd ngfw_upstream
+cd "${THIS_DIR}/ngfw_upstream"
 for i in $(cat build-order.txt | grep -v '#' | grep -v '^\$' | awk '{{print $1}}'); do
 	echo "Building upstream $i"
 	echo
 	echo
-	cd $i
+	cd "${THIS_DIR}/ngfw_upstream/${i}"
 	dpkg-buildpackage
-	cd ..
 	echo "DONE $i"
 	echo
 	echo
