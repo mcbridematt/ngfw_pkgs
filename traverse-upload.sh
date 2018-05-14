238a1ec1c63b3ad83a3da9bb2a8b2fdcce175cc5
Mathew McBride <matt@traverse.com.au>
2018-05-14 11:02:16 +0000

    use rsync instead of ssh for upload

--- traverse-upload.sh
+++ traverse-upload.sh
@@ -7,5 +7,6 @@ else
 fi
 
 echo "${SSH_UPLOAD_KEY}" > upload_user_key
-chmod 0600 upload_user_key 
-scp -o 'HashKnownHosts no' -o 'UserKnownHostsFile upload_host_key' -P "${SSH_UPLOAD_PORT}" -i upload_user_key -r repo "${SSH_UPLOAD_USER}@${SSH_UPLOAD_HOST}:${SSH_UPLOAD_DIR}"
+chmod 0600 upload_user_key
+#scp -o 'HashKnownHosts no' -o 'UserKnownHostsFile upload_host_key' -P "${SSH_UPLOAD_PORT}" -i upload_user_key -r repo "${SSH_UPLOAD_USER}@${SSH_UPLOAD_HOST}:${SSH_UPLOAD_DIR}"
+rsync --delete -e "ssh -o 'HashKnownHosts no' -o 'UserKnownHostsFile upload_host_key' -p ${SSH_UPLOAD_PORT} -i upload_user_key" -r repo/ -v "${SSH_UPLOAD_USER}@${SSH_UPLOAD_HOST}:${SSH_UPLOAD_DIR}"
